391f869af26f3647a78dd2cf4b0e5761aca57008
Adalsteinn <adalsteinn@mariuson.is>
2026-01-23 23:07:19 +0000

    Add Docker Compose for Postgres/Redis and enable experimental SvelteKit features

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,23 @@ import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$features: 'src/lib/features',
+			'$features/*': 'src/lib/features/*'
+		},
+		experimental: {
+			remoteFunctions: true,
+			tracing: { server: true },
+			instrumentation: { server: true }
+		}
+	},
+	compilerOptions: {
+		experimental: {
+			async: true
+		}
+	},
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx']
 };
