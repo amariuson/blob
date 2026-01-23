8e888183368183b3316dfc6357f1751762c6d3ec
Adalsteinn <adalsteinn@mariuson.is>
2026-01-23 22:42:43 +0000

    Install shadcn components

---
+++ src/lib/shared/components/ui/alert-dialog/alert-dialog-action.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { AlertDialog as AlertDialogPrimitive } from 'bits-ui';
+	import { buttonVariants } from '$lib/shared/components/ui/button/index.js';
+	import { cn } from '$lib/shared/utils.js';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		...restProps
+	}: AlertDialogPrimitive.ActionProps = $props();
+</script>
+
+<AlertDialogPrimitive.Action
+	bind:ref
+	data-slot="alert-dialog-action"
+	class={cn(buttonVariants(), className)}
+	{...restProps}
+/>
